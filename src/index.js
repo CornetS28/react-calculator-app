977fa07927f671dfc611f74b413601992601d114
CornetS28 <corsam28@gmail.com>
2019-10-07 18:09:04 -0400

    fixed elsint errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from 'react'; // eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './components/App'; // eslint-disable-line no-unused-vars
 
 ReactDOM.render(<App />, document.getElementById('root'));
