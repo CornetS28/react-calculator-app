45f72b2da48bba8d2ecb60548565415b4c8c97e3
CornetS28 <corsam28@gmail.com>
2019-10-07 14:16:35 -0400

    Updates

---
+++ precache-manifest.0bfb13307bb38864c2a8d038d24d5983.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fc0da6848401a5a7c7d16b883ae701",
+    "url": "/react-calculator-app/index.html"
+  },
+  {
+    "revision": "21ed83e8937712b391fb",
+    "url": "/react-calculator-app/static/css/main.3c076489.chunk.css"
+  },
+  {
+    "revision": "416b73128e1b0577e8e3",
+    "url": "/react-calculator-app/static/js/2.98355da4.chunk.js"
+  },
+  {
+    "revision": "21ed83e8937712b391fb",
+    "url": "/react-calculator-app/static/js/main.5478ba6f.chunk.js"
+  },
+  {
+    "revision": "232754dcd4a9230ceacf",
+    "url": "/react-calculator-app/static/js/runtime-main.7341e873.js"
+  }
+]);
